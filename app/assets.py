94a4914a3957d352cd626baf2c812106b762554a
finncatling <finn.catling@gmail.com>
2020-12-31 09:48:52 +0000

    Added Winsorization thresholds as dict literal

---
+++ app/assets.py
@@ -0,0 +1,12 @@
+WINSOR_THRESHOLDS = {
+    'S01AgeOnArrival': [18.0, 96.0],
+    'S03SerumCreatinine': [20.0, 758.7750000000087],
+    'S03Sodium': [115.0, 157.0],
+    'S03Potassium': [2.3, 7.0],
+    'S03Urea': [1.0, 49.44520000000193],
+    'S03WhiteCellCount': [0.7, 143.0],
+    'S03Pulse': [45.0, 173.25900000002002],
+    'S03SystolicBloodPressure': [60.0, 214.0],
+    'S03PreOpLowestAlbumin': [3.9712000000000005, 73.0],
+    'S03PreOpArterialBloodLactate': [0.3, 19.0]
+}

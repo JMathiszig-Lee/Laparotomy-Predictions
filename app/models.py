51ddbc2e00a3e897855beefa33f3041cbec03f76
Finn Catling <finn.catling@gmail.com>
2022-06-09 14:58:58 +0100

    Added more detail to schema

--- app/models.py
+++ app/models.py
@@ -1,29 +1,92 @@
+from typing import List, Optional, Union
+
 from pydantic import BaseModel, Field
-from typing import Optional, List
 
 
 class Prediction(BaseModel):
     """model to define inputs for prediction"""
 
-    Age: int 
-    ASA: int = Field(ge=0, le=4, default=None, title="ASA grade", description="encoded -1")
-    HR: int = Field(title="Heart Rate")
-    SBP: int = Field(title="Systolic Blood Pressure", description="systolic blood pressure in mmHg")
-    WCC: float = Field(title="White Cell Count", description="white cell count x10^9/L")
-    Na: int = Field(title="Sodium", description="units: mmol/L")
-    K: float = Field(title="Potassium", description="units: mmol/L")
-    Urea: float = Field(title="Urea", description="units: mmol/L")
-    Creat: int = Field(title="Creatanine", description="units: umol/L")
-    Lactate: Optional[float] = Field(title="Lactate", description="units: mmol/L")
-    Albumin: Optional[int] = Field(title="Albumin", description="units: g/L")
+    Age: int = Field(ge=0, description="Units: years")
+    ASA: int = Field(
+        ge=0,
+        le=4,
+        title="ASA grade",
+        description="Encoded as ASA grade, minus 1",
+    )
+    HR: int = Field(title="Heart Rate", description="Units: Beats per minute")
+    SBP: int = Field(
+        title="Systolic Blood Pressure", description="Units: mmHg"
+    )
+    WCC: float = Field(title="White Cell Count", description="Units: x 10^9/L")
+    Na: int = Field(title="Sodium", description="Units: mmol/L")
+    K: float = Field(title="Potassium", description="Units: mmol/L")
+    Urea: float = Field(title="Urea", description="Units: mmol/L")
+    Creat: int = Field(title="Creatinine", description="Units: μmol/L")
+    Lactate: Optional[float] = Field(
+        title="Lactate", description="Units: mmol/L"
+    )
+    Albumin: Optional[int] = Field(title="Albumin", description="Units: g/L")
     GCS: int = Field(title="Glasgow Coma Score", ge=3, le=15)
-    Resp: int
-    Cardio: int
-    Arrhythmia: bool = Field(title="Arrythmia", description="Does the patient have an arrhytmia?")
-    CT_performed: bool = Field(description="Did the patient have a CT pre-operatively?")
-    Indication: int
-    Malignancy: int
-    Soiling: int
+    Resp: int = Field(
+        title="Respiratory status",
+        ge=0,
+        le=3,
+        description=(
+            "Encoded as: 0=No dyspnoea, 1=Mild COPD or dyspnoea, 2=Moderate "
+            "COPD or dyspnoea, 3=Fibrosis or consolidation or severe dyspnoea"
+        ),
+    )
+    Cardio: int = Field(
+        title="Cardiovascular status",
+        ge=0,
+        le=3,
+        description=(
+            "Encoded as: 0: No cardiac failure, 1: Cardiovascular medications, "
+            "2: Peripheral oedema or taking warfarin, 3: Raised jugular venous "
+            "pressure or cardiomegaly"
+        ),
+    )
+    Arrhythmia: bool = Field(
+        title="Arrhythmia",
+        description=(
+            "Does the patient have an arrhythmia? (Excludes sinus tachycardia. "
+            "Includes rate-controlled atrial fibrillation.)"
+        ),
+    )
+    CT_performed: bool = Field(
+        description=(
+            "Did the patient have a CT abdomen and pelvis pre-operatively?"
+        )
+    )
+    Indication: int = Field(
+        title="Indication for surgery",
+        ge=0,
+        le=12,
+        description=(
+            "Encoded as: 0=Small bowel obstruction, 2=Perforation, 3=Large "
+            "bowel obstructtion, 4=Peritonitis, 5=Ischaemia, 6=Haemorrhage, "
+            "7=Colitis, 8=Other, 9=Abdominal abscess, 10=Anastamotic leak, "
+            "11=Incarcerated hernia, 12=Volvulus"
+        ),
+    )
+    Malignancy: int = Field(
+        title="Does the patient have cancer? If so, to what extent?",
+        ge=0,
+        le=3,
+        description=(
+            "Encoded as: 0=None, 1=Primary only, 2=Nodal metastasis, 3=Distant "
+            "metastasis"
+        ),
+    )
+    Soiling: int = Field(
+        title="Is their peritoneal soiling? If so, to what extent?",
+        ge=0,
+        le=3,
+        description=(
+            "Encoded as: 0=None, 1=Serous fluid, 2=Local pus, 3=Free bowel "
+            "content, pus or blood"
+        ),
+    )
 
     class Config:
         schema_extra = {
